25f486e98183408befcc311dc4764fffe3cf6097
Fernando Mora <f.mora.g90@gmail.com>
2022-08-17 11:57:13 -0400

    fix(motivus): update setup paths

--- motivus_setup.sh
+++ motivus_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 # copy compiled algorithm into base dir
+mkdir -p pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/build
+mkdir -p pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/runa/build
 cp build/mypmmoda-*.js pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/build/
 cp build/mypmmoda-*.wasm pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/build/
-cp build/mypmmoda-*.js pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/nruna/build/
-cp build/mypmmoda-*.wasm pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/nruna/build/
+cp build/mypmmoda-*.js pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/runa/build/
+cp build/mypmmoda-*.wasm pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/runa/build/
 
 # make driver.py mypyDAMPF.py module and configure motivus
 cp driver.py pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/mypyDAMPF.py
@@ -13,6 +15,6 @@ cp .env pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/
 cp .env pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/runa/
 
 # remove system dependant compiled binaries
-rm EXECUTE_pyDAMPF/mypyDAMPF.cpython-39-x86_64-linux-gnu.so ;
-rm EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/mypyDAMPF.cpython-39-x86_64-linux-gnu.so ;
-rm EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/runa/mypyDAMPF.cpython-39-x86_64-linux-gnu.so ;
+rm pyDAMPF/EXECUTE_pyDAMPF/mypyDAMPF.cpython-39-x86_64-linux-gnu.so ;
+rm pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/mypyDAMPF.cpython-39-x86_64-linux-gnu.so ;
+rm pyDAMPF/EXECUTE_pyDAMPF/pyDAMPF_BASE/nrun/runa/mypyDAMPF.cpython-39-x86_64-linux-gnu.so ;
